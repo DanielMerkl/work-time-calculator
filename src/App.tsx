ab19d90553d85f19956261019c1152e7d080f13d
Daniel Merkl <daniel.merkl.dm@gmail.com>
2019-07-27 20:37:41 +0200

    BottomNavigation implementiert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from "react";
 import ApplicationBar from "./components/ApplicationBar";
+import BottomNavigation from "./components/BottomNavigation";
 
 const App: FC = () => {
-  return <ApplicationBar />;
+  return (
+    <>
+      <ApplicationBar />
+      <BottomNavigation />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/BottomNavigation.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useState } from "react";
+import {
+  BottomNavigation as MuiBottomNavigation,
+  BottomNavigationAction
+} from "@material-ui/core";
+import { Alarm, Settings, AccountCircle } from "@material-ui/icons";
+import { useScreenSize } from "../utils/useScreenSize";
+
+const BottomNavigation: FC = () => {
+  const [navIndex, setNavIndex] = useState(0);
+  const { isLargeDevice } = useScreenSize();
+
+  if (isLargeDevice) return null;
+
+  return (
+    <MuiBottomNavigation
+      value={navIndex}
+      onChange={(e, newNavIndex) => setNavIndex(newNavIndex)}
+      showLabels
+      style={{}}
+    >
+      <BottomNavigationAction label="Rechner" icon={<Alarm />} />
+      <BottomNavigationAction label="Einstellungen" icon={<Settings />} />
+      <BottomNavigationAction label="Einloggen" icon={<AccountCircle />} />
+    </MuiBottomNavigation>
+  );
+};
+
+export default BottomNavigation;
