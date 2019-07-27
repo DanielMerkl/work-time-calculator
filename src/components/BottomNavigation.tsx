918d8c4e70328b6e37b7f4d42ab89310f0e7d8c7
Daniel Merkl <daniel.merkl.dm@gmail.com>
2019-07-27 21:42:46 +0200

    Navigation in BottomNavigation implementiert

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,21 +1,56 @@
-import React, { FC, useState } from "react";
+import React, { ChangeEvent, FC } from "react";
 import {
   BottomNavigation as MuiBottomNavigation,
   BottomNavigationAction
 } from "@material-ui/core";
-import { Alarm, Settings, AccountCircle } from "@material-ui/icons";
+import { AccountCircle, Alarm, Settings } from "@material-ui/icons";
 import { useScreenSize } from "../utils/useScreenSize";
+import useRouter from "use-react-router";
+import routes from "../utils/routes";
+
+enum NavIndex {
+  Calculation,
+  Settings,
+  Auth
+}
 
 const BottomNavigation: FC = () => {
-  const [navIndex, setNavIndex] = useState(0);
   const { isLargeDevice } = useScreenSize();
+  const { history, location } = useRouter();
 
   if (isLargeDevice) return null;
 
+  const calculateNavIndex = (): NavIndex => {
+    switch (location.pathname) {
+      case routes.calculation:
+        return NavIndex.Calculation;
+      case routes.settings:
+        return NavIndex.Settings;
+      case routes.auth:
+        return NavIndex.Auth;
+      default:
+        return NavIndex.Calculation;
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<{}>, newNavIndex: NavIndex) => {
+    switch (newNavIndex) {
+      case NavIndex.Calculation:
+        history.push(routes.calculation);
+        break;
+      case NavIndex.Settings:
+        history.push(routes.settings);
+        break;
+      case NavIndex.Auth:
+        history.push(routes.auth);
+        break;
+    }
+  };
+
   return (
     <MuiBottomNavigation
-      value={navIndex}
-      onChange={(e, newNavIndex) => setNavIndex(newNavIndex)}
+      value={calculateNavIndex()}
+      onChange={handleChange}
       showLabels
     >
       <BottomNavigationAction label="Rechner" icon={<Alarm />} />
