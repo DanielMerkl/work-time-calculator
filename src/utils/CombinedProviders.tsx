311830562f0dfd5daa840502c7e9cc5083fb8e2d
Daniel Merkl <daniel.merkl.dm@gmail.com>
2019-08-01 16:41:44 +0200

    material-ui/pickers hinzugefügt und konfiguriert

--- src/utils/CombinedProviders.tsx
+++ src/utils/CombinedProviders.tsx
@@ -2,11 +2,17 @@ import React, { FC } from "react";
 import { ThemeProvider } from "@material-ui/styles";
 import { BrowserRouter } from "react-router-dom";
 import theme from "./theme";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
 
 const CombinedProviders: FC = ({ children }) => {
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          {children}
+        </MuiPickersUtilsProvider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
