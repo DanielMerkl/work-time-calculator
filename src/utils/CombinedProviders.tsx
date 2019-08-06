de284414207e647314ea990ac73a8bfa44bc21e7
Daniel Merkl <daniel.merkl.dm@gmail.com>
2019-08-06 11:06:13 +0200

    InstallationContext implementiert

--- src/utils/CombinedProviders.tsx
+++ src/utils/CombinedProviders.tsx
@@ -3,18 +3,21 @@ import { ThemeProvider } from "@material-ui/styles";
 import { BrowserRouter } from "react-router-dom";
 import ThemeContextProvider, { ThemeContext } from "../context/ThemeContext";
 import { CssBaseline } from "@material-ui/core";
+import InstallationContextProvider from "../context/InstallationContext";
 
 const CombinedProviders: FC = ({ children }) => (
   <BrowserRouter>
     <ThemeContextProvider>
-      <ThemeContext.Consumer>
-        {themeContext => (
-          <ThemeProvider theme={themeContext.theme}>
-            <CssBaseline />
-            {children}
-          </ThemeProvider>
-        )}
-      </ThemeContext.Consumer>
+      <InstallationContextProvider>
+        <ThemeContext.Consumer>
+          {themeContext => (
+            <ThemeProvider theme={themeContext.theme}>
+              <CssBaseline />
+              {children}
+            </ThemeProvider>
+          )}
+        </ThemeContext.Consumer>
+      </InstallationContextProvider>
     </ThemeContextProvider>
   </BrowserRouter>
 );
